d88cf8895c5b29d7c8e474819b61d7ab380aa447
Fariborz Afkari <fariborz_afkari@hotmail.com>
2026-09-26 22:18:40 +0330

    Add files via upload

---
+++ Presentation/Game/CardViewData.cs
@@ -0,0 +1,21 @@
+﻿namespace CardGame.Presentation.Game
+{
+    public sealed class CardViewData
+    {
+        public int InstanceId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public int Value { get; private set; }
+
+        public CardViewData(
+            int instanceId,
+            string name,
+            int value)
+        {
+            InstanceId = instanceId;
+            Name = name;
+            Value = value;
+        }
+    }
+}
---
+++ Presentation/Game/GameViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CardGame.Presentation.Game
+{
+    public sealed class GameViewModel
+    {
+        public int Coins { get; set; }
+
+        public bool CanStartMatch { get; set; }
+
+        public bool IsPlayerTurn { get; set; }
+
+        public bool IsGameOver { get; set; }
+
+        public string CurrentPlayerName { get; set; }
+
+        public string Status { get; set; }
+
+        public IReadOnlyList<GamePresenter.CardViewData> Hand { get; set; }
+    }
+}
